2fc1e1d9e5e26978e5a167cb0ff539c15df8d2cb
JANGWOO JUNG <81564742+Smallt0wn@users.noreply.github.com>
2025-05-08 15:45:06 +0900

    add file
    add HW02 file

--- HW02.ino
+++ HW02.ino
@@ -1,17 +1,28 @@
-// Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
-const int potPin = 34;
+const int buttonPin = 4;  // the number of the pushbutton pin
+const int ledPin =  5;    // the number of the LED pin
 
-// variable for storing the potentiometer value
-int potValue = 0;
+// variable for storing the pushbutton status 
+int buttonState = 0;
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
+  Serial.begin(115200);  
+  // initialize the pushbutton pin as an input
+  pinMode(buttonPin, INPUT);
+  // initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // Reading potentiometer value
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
-  delay(500);
+  // read the state of the pushbutton value
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH
+  if (buttonState == HIGH) {
+    // turn LED on
+    digitalWrite(ledPin, LOW);
+  } else {
+    // turn LED off
+    digitalWrite(ledPin, HIGH);
+  }
 }
